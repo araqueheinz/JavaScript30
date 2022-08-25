854bc71f0c08c58a42f5c5f03e80eb5e7e36fb24
Heinz Araque <heinz@defenage.com>
2022-08-25 12:05:00 -0700

    10 - Hold Shift to Check Multiple Checkboxes

---
+++ 10-Hold Shift to Check multiple Checkboxes/main.js
@@ -0,0 +1,19 @@
+const checkboxes = document.querySelectorAll('.item input[type="checkbox"]');
+function handleCheck(e) {
+    let inBetween = false;
+    if(e.shiftKey && this.checked){
+        checkboxes.forEach(checkbox => {
+            if(checkbox === this || checkbox === lastChecked){
+                inBetween = !inBetween
+            }
+            if (inBetween){
+                checkbox.checked =  true
+            }
+       })
+    }
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', handleCheck)
+})
